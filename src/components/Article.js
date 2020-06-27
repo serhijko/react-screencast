5c70efd52158261ae786ef711b4a3f85de0481e9
serhijko <sp_by@tut.by>
2020-06-27 19:29:17 +0300

    react css

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,24 +3,31 @@ import React, { useState } from 'react';
 function Article(props) {
     const [isOpen, setIsOpen] = useState(true);
 
-    const { article } = props;
-    const body = isOpen && <section>{article.text}</section>;
-
     const handleClick = () => {
         console.log('---', 'clicked');
         setIsOpen(!isOpen);
     };
 
+    const { article } = props;
+    const body = isOpen && <section className="card-text">{article.text}</section>;
+    const style = { width: '50%' };
+
     return (
-        <div>
-            <h2>
-                {article.title}
-                <button onClick={handleClick}>
-                    {isOpen ? 'close' : 'open'}
-                </button>
-            </h2>
-            {body}
-            <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
+        <div className="card mx-auto" style={style}>
+            <div className="card-header">
+                <h2>
+                    {article.title}
+                    <button onClick={handleClick} className="btn btn-primary btn-lg float-right">
+                        {isOpen ? 'close' : 'open'}
+                    </button>
+                </h2>
+            </div>
+            <div className="card-body">
+                <h6 className="card-subtitle text-muted">
+                    creation date: {(new Date(article.date)).toDateString()}
+                </h6>
+                {body}
+            </div>
         </div>
     );
 }
