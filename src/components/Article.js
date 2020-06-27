59ea712aa13912f13c18f117f5ee4070bfccefcb
serhijko <sp_by@tut.by>
2020-06-27 17:34:38 +0300

    react state

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Article(props) {
+    const [isOpen, setIsOpen] = useState(true);
+
     const { article } = props;
-    console.log('---', props);
-    const body = <section>{article.text}</section>;
+    const body = isOpen && <section>{article.text}</section>;
+
+    const handleClick = () => {
+        console.log('---', 'clicked');
+        setIsOpen(!isOpen);
+    };
+
     return (
         <div>
-            <h2>{article.title}</h2>
+            <h2>
+                {article.title}
+                <button onClick={handleClick}>
+                    {isOpen ? 'close' : 'open'}
+                </button>
+            </h2>
             {body}
             <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
         </div>
